338ad86c4911358bde0c2e3cafec5c01fa74414c
Pranjal Tyagi <pranjaltyagi@gmail.com>
2025-07-31 13:08:44 +0530

    api_url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://backend:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  plugins: [react(), tailwindcss()]
+
 })
